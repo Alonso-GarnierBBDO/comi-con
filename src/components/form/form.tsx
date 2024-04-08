76e00a377b7bc87cb9e566b033cf88409ffef466
Alonso-GarnierBBDO <alonso.artavia@garnierbbdo.com>
2024-04-08 15:09:01 -0600

    Se agrego el sitio

--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,4 +1,4 @@
-import ChoysChocolateImage from '../../assets/img/choys_chocolate.png';
+//import ChoysChocolateImage from '../../assets/img/choys_chocolate.png';
 import ChoysLogo from '../../assets/img/choys_logo.png';
 import RemolinoImg from '../../assets/img/remolino.png';
 import AudioSound from '../../helper/audio/audio';
@@ -100,11 +100,11 @@ const Form = ({ removeForm }: Props) => {
                 <section className='content_items'>
                     <section className='content'>
                         <h2>¡PRIMERO, VAMOS A CONOCER EL PERFIL DE TU PERSONAJE!</h2>
-                        <section className='container_remolino'>
+                        <section className='image'>
+                            <section className='container_remolino'>
                                 <img className='remolino' src={RemolinoImg} alt="Remolino dan vueltas" />
                             </section>
-                        <section className='image'>
-                            <img className='image_chocolate' src={ChoysChocolateImage} alt="Chocolate Imagen" />
+                            
                         </section>
                     </section>
                     <form onSubmit={ (e) => save_form(e) }>
